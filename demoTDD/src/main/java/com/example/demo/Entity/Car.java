b9c871e04eda9e7a19b9e0b7f5871e6729d9aaea
Ra8521 <taraon71@gmail.com>
2021-05-12 17:40:26 +0530

    test added

---
+++ demoTDD/src/main/java/com/example/demo/Entity/Car.java
@@ -0,0 +1,17 @@
+package com.example.demo.Entity;
+
+import java.util.function.IntPredicate;
+
+public class Car {
+
+	public IntPredicate getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public IntPredicate getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
