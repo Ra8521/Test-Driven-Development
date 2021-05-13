1db5a431c1567b403e9eb08adc5d847f85ead5ac
Ra8521 <taraon71@gmail.com>
2021-05-13 08:41:46 +0530

    update

--- demoTDD/src/main/java/com/example/demo/controller/CarController.java
+++ demoTDD/src/main/java/com/example/demo/controller/CarController.java
@@ -1,5 +1,18 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Entity.Car;
+
+@RestController
 public class CarController {
+	
+	@GetMapping("/cars/{name}")
+	private Car getCar(@PathVariable String name){
+		return null;
+		
+	}
 
 }
